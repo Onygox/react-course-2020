687862f858610cad83af7968bcd3ab9c46aa0d9d
Lionel Miele-Herndon <miell534@newschool.edu>
2020-10-21 21:32:51 -0400

    firebase

---
+++ hunt-test/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAosH0vSONUfcAsxiezFgbfErgFa1GK1Ao",
+  authDomain: "hunt-test-6bea2.firebaseapp.com",
+  databaseURL: "https://hunt-test-6bea2.firebaseio.com",
+  projectId: "hunt-test-6bea2",
+  storageBucket: "hunt-test-6bea2.appspot.com",
+  messagingSenderId: "1020105927517",
+  appId: "1:1020105927517:web:5df0eb03809027f8bc96b4"
+};
+
+export default firebaseConfig;
