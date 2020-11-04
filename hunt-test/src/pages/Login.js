bfb7f0334e6c1aaafbc4a19f386fe2ccd8334674
Lionel Miele-Herndon <miell534@newschool.edu>
2020-11-04 13:02:21 -0500

    login authentication

--- hunt-test/src/pages/Login.js
+++ hunt-test/src/pages/Login.js
@@ -1,39 +1,99 @@
-import React from 'react';
-import {IonPage, IonItem, IonLabel, IonRow, IonCol, IonButton, IonRouterLink, IonContent, IonInput} from '@ionic/react';
-import NavHeader from '../components/Header/NavHeader.js';
+import React from "react";
+import {
+  IonPage,
+  IonContent,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonRouterLink,
+  IonLoading,
+} from "@ionic/react";
+import { toast } from "../utils/toast";
+import useFormValidation from "../hooks/useFormValidation";
+import validateLogin from "../components/Auth/validateLogin";
+import firebase from "../firebase";
+import NavHeader from "../components/Header/NavHeader";
 
-const Login = () => {
-    return (
-        <IonPage>
-            <NavHeader title="Log In" />
-            <IonContent>
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+};
 
-                <IonItem lines="full">
-                    <IonLabel position = "floating">Email</IonLabel>
-                    <IonInput name="email" type="text" required></IonInput>
-                </IonItem>
+const Login = (props) => {
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    isSubmitting,
+  } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser);
+  const [busy, setBusy] = React.useState(false);
 
-                <IonItem lines="full">
-                    <IonLabel position = "floating">Password</IonLabel>
-                    <IonInput name="password" type="password" required></IonInput>
-                </IonItem>
+  async function authenticateUser() {
+    setBusy(true);
+    const { email, password } = values;
+    try {
+      await firebase.login(email, password);
+      toast("You have logged in successfully!");
+      props.history.push("/");
+    } catch (err) {
+      console.error("Authentication Error", err);
+      toast(err.message);
+    }
+    setBusy(false);
+  }
 
-                <IonRow>
-                    <IonCol>
-                        <IonButton type="submit" color="primary" expand="block">Log In</IonButton>
-                    </IonCol>
-                </IonRow>
+  return (
+    <IonPage>
+      <NavHeader title="Log In" />
+      <IonLoading message={"Please wait..."} isOpen={busy} />
+      <IonContent>
+        <IonItem lines="full">
+          <IonLabel position="floating">Email</IonLabel>
+          <IonInput
+            name="email"
+            value={values.email}
+            type="text"
+            onIonChange={handleChange}
+            required
+          ></IonInput>
+        </IonItem>
+        <IonItem lines="full">
+          <IonLabel position="floating">Password</IonLabel>
+          <IonInput
+            name="password"
+            type="password"
+            value={values.password}
+            onIonChange={handleChange}
+            required
+          ></IonInput>
+        </IonItem>
 
-                <IonRow>
-                    <IonCol>
-                        <IonRouterLink routerLink={'/forgot'}>
-                            Forgot Password?
-                        </IonRouterLink>
-                    </IonCol>
-                </IonRow>
-            </IonContent>
-        </IonPage>
-    );
+        <IonRow>
+          <IonCol>
+            <IonButton
+              type="submit"
+              color="primary"
+              expand="block"
+              onClick={handleSubmit}
+              disabled={isSubmitting}
+            >
+              Log In
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol class="ion-text-center ion-padding-vertical">
+            <IonRouterLink routerLink={`/forgot`}>
+              Forgot Password?
+            </IonRouterLink>
+          </IonCol>
+        </IonRow>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Login;
